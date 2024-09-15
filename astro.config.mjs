dd142eb170710055070c118fc1932443a3bdcb26
DJMZILA <djm.zila@outlook.com>
2024-09-15 02:32:53 +0200

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import alpinejs from '@astrojs/alpinejs';
+
+import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [alpinejs(), tailwind(), icon()]
+});
